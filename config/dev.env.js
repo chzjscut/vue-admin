faad8184f8bdce05ed126cb837d23a10d6d51b50
Chen Zijing <chzjstar@126.com>
2019-06-25 17:32:43 +0800

    Add files via upload

---
+++ config/dev.env.js
@@ -0,0 +1,6 @@
+module.exports = {
+    NODE_ENV: '"development"',
+    ENV_CONFIG: '"dev"',
+    BASE_API: '"http://localhost:3000"',
+    APP_ORIGIN: '"https://wallstreetcn.com"'
+}
---
+++ config/prod_test.env.js
@@ -0,0 +1,6 @@
+module.exports = {
+    NODE_ENV: '"production"',
+    ENV_CONFIG: '"prod"',
+    BASE_API: '"http://swdev.suconnected.com"',
+    APP_ORIGIN: '"https://wallstreetcn.com"'
+};
